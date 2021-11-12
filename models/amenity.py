a392c5eda67543ea3b7c9437b7710912b564be79
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-11-12 10:29:03 -0500

    public class atribute, name, sting, empty string

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class than inherits from BaseModel"""
+    name = ""
