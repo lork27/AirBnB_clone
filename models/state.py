96027c167443498b4ef62d4bd0efc8b0a5430e19
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-11-12 12:12:34 -0500

    public class atributte, name, string and empty string

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains State class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """state class than inherits from BaseModel"""
+    name = ""
