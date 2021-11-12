d65527d80b8812ac4b502de5601a942132fdb8ae
lork27 <g.lorca.lamadrid@gmail.com>
2021-11-12 19:03:32 -0400

    ADDED show and all methods to the console

--- console.py
+++ console.py
@@ -1,19 +1,73 @@
 #!/usr/bin/env python3
-
 import cmd
+import json
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+
+classes_dict = {"BaseModel": BaseModel,
+                "User": User,
+                "City": City,
+                "Place": Place,
+                "State": State,
+                "Review": Review,
+                "Amenity": Amenity,
+                }
+
+list_of_classes = list(classes_dict.keys())
+objs = storage.all()
 
 
 class HBNBCommand(cmd.Cmd):
     """
     HBNBCommand - command line interface implementation
     """
-
-    # intro = "Welcome to the hbnb console"
     prompt = "(hbnb) "
     file = None
 
+    def do_all(self, arg):
+        """
+        print str representation of all instances
+        based on name
+        """
+        if len(arg) == 0:
+            # print(objs)
+            for key in objs.keys():
+                obj = objs[key]
+                print(obj)
+        elif arg in list_of_classes:
+            for key in objs.keys():
+                if arg in key:
+                    obj = objs[key]
+                    print(obj)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        prints str representation of obj based in id
+        """
+        split_arg = arg.split(" ")
+        if len(arg) == 0:
+            print("** class is missing **")
+        else:
+            if split_arg[0] not in list_of_classes:
+                print("** class doesn't exist **")
+            elif len(split_arg) < 2:
+                print("** instance id is missing")
+            else:
+                try:
+                    print(objs[split_arg[0] + "." + split_arg[1]])
+                except:
+                    print("** no instance found **")
+    '''basic commands below'''
+
     def do_quit(self, line):
-        # print("Thankyou for using hbnb console")
         self.close()
         quit()
 
