5a60fbe3ab4a72590bcf0ff5be2c014a377849e4
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-11-12 12:22:31 -0500

    public, state_id, name

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Module that contains City class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """state class than inherits from BaseModel"""
+    state_id = ""
+    name = ""
