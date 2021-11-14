f80ed5e61c88a65db9914c484f4f055ce1354a32
lork27 <g.lorca.lamadrid@gmail.com>
2021-11-13 22:23:37 -0400

    added docstrings

--- models/city.py
+++ models/city.py
@@ -5,6 +5,6 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """state class than inherits from BaseModel"""
+    """city class than inherits from BaseModel"""
     state_id = ""
     name = ""
